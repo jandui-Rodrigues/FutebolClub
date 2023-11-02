adbee877a189cf6da47f5757961d63e9593f37a0
jandui <janduineto18@gmail.com>
2023-11-02 11:45:05 -0300

    test(Get): add test unitary for route get /matches

--- app/backend/src/tests/Matcher.test.ts
+++ app/backend/src/tests/Matcher.test.ts
@@ -5,18 +5,29 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app'
 import SequelizeMatcher from '../database/models/SequelizeMatches';
-import { matches } from './mock/matcher/Matcher.mock';
+import * as matcherMock from './mock/matcher/Matcher.mock';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Team Test', function() {
-  it('should return all teams', async function() {
+describe('Matcher Test', function() {
+  it('should return all matchers', async function() {
     // Arrange
+    sinon.stub(SequelizeMatcher, 'findAll').resolves(matcherMock.matches as any);
+    // Act
+    const { status, body } = await chai.request(app).get('/matches');
+    // Assert
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(matcherMock.matches);
+  });
+
+  it('should return all matchers inprogress true', async function() {
+    // Arrange
+    const matches = matcherMock.matches.filter(match => match.inProgress === true);
     sinon.stub(SequelizeMatcher, 'findAll').resolves(matches as any);
     // Act
-    const { status, body } = await chai.request(app).get('/teams');
+    const { status, body } = await chai.request(app).get('/matches?inProgress=true');
     // Assert
     expect(status).to.equal(200);
     expect(body).to.deep.equal(matches);
