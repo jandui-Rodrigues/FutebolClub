dd6433c811fb7e456c2af6c897d948d5cd2c08aa
jandui <janduineto18@gmail.com>
2023-11-16 12:57:40 -0300

    chore: add scripts in package

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,8 +2,8 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: 'TRYBE_FUTEBOL_CLUBE',
+  password: process.env.DB_PASS || 'suaSenha',
+  database: 'FUTEBOL_CLUBE',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
